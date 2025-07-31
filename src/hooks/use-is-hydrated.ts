3641bae53abd82da573f9fd4cbe1d17203dd795c
Lukas Machado <lukaskmachado@gmail.com>
2025-07-31 02:23:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-is-hydrated.ts
+++ src/hooks/use-is-hydrated.ts
@@ -1,21 +1,12 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useGameStore } from "@/store/game-store";
 
 export const useIsHydrated = () => {
   const [isHydrated, setIsHydrated] = useState(false);
 
   useEffect(() => {
-    const unsub = useGameStore.persist.onHasHydrated(() => {
-      setIsHydrated(true);
-    });
-
-    setIsHydrated(useGameStore.persist.hasHydrated());
-
-    return () => {
-      unsub();
-    };
+    setIsHydrated(true);
   }, []);
 
   return isHydrated;
